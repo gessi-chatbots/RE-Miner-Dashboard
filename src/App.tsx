e95e2c1a10dc44493b3eb7c580ad83d6b593b42c
mtiessler <max.tiessler@estudiantat.upc.edu>
2024-01-21 17:16:01 +0100

    added amplify auth in the end

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { Amplify } from 'aws-amplify';
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import config from './amplifyconfiguration.json';
 import React from 'react';
 import AppRoutes from "./routes/AppRoutes";
 import '../src/scss/app.scss'
 
-
-function App() {
+Amplify.configure(config);
+export function App({ signOut, user }: WithAuthenticatorProps) {
   return <AppRoutes />
 }
 
-export default App;
+export default withAuthenticator(App);
