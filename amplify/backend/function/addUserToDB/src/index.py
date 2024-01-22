d64f8b5ce2474da7db317112f0abc108a7f8642f
mtiessler <max.tiessler@estudiantat.upc.edu>
2024-01-22 16:07:22 +0100

    first integration cognito - dynamodb done

--- amplify/backend/function/addUserToDB/src/index.py
+++ amplify/backend/function/addUserToDB/src/index.py
@@ -1,15 +1,22 @@
 import json
+import boto3
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+    database = boto3.resource('dynamodb')
+    table = database.Table('users-dev')
+
+    table.put_item(
+        Item={
+            'email': event['request']['userAttributes']['email']
+        }
+    )
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps(event)
+    }
