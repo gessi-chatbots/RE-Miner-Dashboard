7763dfb6ce33128b00085de44b230101343fae59
mtiessler <max.tiessler@estudiantat.upc.edu>
2024-01-22 21:12:20 +0100

    removed pre aws api architecture

--- src/api/hooks/index.tsx
+++
@@ -1,3 +0,0 @@
-import useUserHook from './useUserHook';
-
-export {  useUserHook };
--- src/api/hooks/useUserHook.tsx
+++
@@ -1,8 +0,0 @@
-import {APICore} from "../apiCore";
-
-export default function useUserHook() {
-    const api = new APICore();
-
-    const loggedInUser = api.getLoggedInUser();
-    return [loggedInUser];
-}
