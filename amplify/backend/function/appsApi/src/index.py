955998a830b1f591e6f0c7310c105850c8fcf75a
mtiessler <max.tiessler@estudiantat.upc.edu>
2024-01-25 11:38:48 +0100

    feat(Create apps): Implemented POST method

--- amplify/backend/function/appsApi/src/index.py
+++ amplify/backend/function/appsApi/src/index.py
@@ -1,15 +1,35 @@
 import json
+import awsgi
+import boto3
+from flask_cors import CORS
+from flask import Flask, jsonify, request
+from uuid import uuid4
+
+app = Flask(__name__)
+CORS(app)
+BASE_ROUTE = "/apps"
+client = boto3.client("dynamodb")
+TABLE = "applications-dev"
+
+@app.route(BASE_ROUTE, methods=['GET'])
+def list_apps():
+    return jsonify(message="app list ;P")
+
+@app.route(BASE_ROUTE, methods=['POST'])
+def create_apps():
+    request_json = request.get_json()
+    client.put_item(TableName=TABLE, Item={
+        'id': {'S': str(uuid4())},
+        'name': {'S': request_json.get("app_name")},
+        'description': {'S': request_json.get("description")},
+        'summary': {'S': request_json.get("summary")},
+        'release_date': {'S': request_json.get("release_date")},
+        'version': {'I': request_json.get("version")}
+    })
+    return jsonify(message="item created")
 
 def handler(event, context):
   print('received event:')
   print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+
+  return awsgi.response(app, event, context)
