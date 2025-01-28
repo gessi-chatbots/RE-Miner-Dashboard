0de76351c1e48239cf64004a63fc365842583ebf
mtiessler <max.tiessler@upc.edu>
2025-01-28 10:50:34 +0100

    refactor DTO

--- src/DTOs/ReviewManagerDTO.ts
+++ src/DTOs/ReviewManagerDTO.ts
@@ -2,7 +2,7 @@ export interface ReviewManagerDTO {
     app_id: string;
     app_package: string;
     app_name: string;
-    reviewId: string;
+    review_id: string;
     review: string;
     language_model: string;
     polarities: string[];
